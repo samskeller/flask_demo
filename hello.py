f25a9c735ca116ae2d9a6a7937288f6a69391d86
Sam Keller <samskeller@gmail.com>
2015-05-25 21:16:13 -0700

    Learning how to make basic forms with flask-wtf

--- hello.py
+++ hello.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 from datetime import datetime
 
 app = Flask(__name__)
+with open('secret_key') as f:
+    app.config['SECRET_KEY'] = f.readline().strip()
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 manager = Manager(app)
@@ -18,13 +23,24 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
 
 if __name__ == '__main__':
     manager.run()
